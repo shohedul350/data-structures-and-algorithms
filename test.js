2227ea575ff6b8a09f4443ed1cd213a53ae31e19
shohedul350 <shohedulislamemon@gmail.com>
2020-07-14 21:25:33 +0600

    selection sort

--- test.js
+++ test.js
@@ -57,3 +57,32 @@
  
 // //   binarySearch(sortedRainbow, "green"); // returns 1
 //   binarySearch(sortedRainbow, "white") // returns not found
+
+
+
+// selection sort
+
+// let arr=[ 4,3,5,2]
+// function selectionSort(arr){
+//     for (var i = 0; i < arr.length; i++) {
+
+//         let min = i; //  storing the index of minimum element
+ 
+//         for (var j = i + 1; j < arr.length; j++) {
+//           if (arr[min] > arr[j]) {
+//                 min = j; // updating the index of minimum element
+              
+//             }
+           
+//         }
+      
+//         if (i !== min) {
+//             let temp = arr[ i ];
+//             arr[ i ] = arr[min];
+//             arr[min] = temp;
+//         }
+//     }
+//     return arr;
+//   }
+// console.log(selectionSort(arr))
+  
